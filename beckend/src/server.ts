bae07bc46afa50d78c43a22370db9a474f8129f3
marcelorafael <marcelorafael23@hotmail.com>
2022-09-13 20:52:57 -0300

    feat: base API created.

---
+++ beckend/src/server.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response, NextFunction } from "express";
+
+import { router } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+
+// ROTEAMENTO
+app.use(router);
+
+app.listen(3333, () => {
+  console.log("servidor online");
+});
